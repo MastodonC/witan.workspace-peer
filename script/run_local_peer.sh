8d63ddc4f1900e779aa1a3db0f9b7e8e77df77a1
Antony Woods <antony@mastodonc.com>
2016-06-02 10:58:04 +0100

    Fix docker run script

--- script/run_local_peer.sh
+++ script/run_local_peer.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -p 8080:8080 -e "ONYX_ID=1" -e "NPEERS=1" --link "zookeeper:zk" --privileged -it mastodonc/witan.workspace-peer:$(lein project-version)
+docker run -p 10015:10015 -e "ONYX_ID=1" -e "NPEERS=1" --link "zookeeper:zk" --privileged -it mastodonc/witan.workspace-peer:$(lein project-version)
