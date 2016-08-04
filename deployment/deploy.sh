fd14cb72cfb4294b50f1f853a5cd34cb0877a3ac
Tom Coupland <tcoupland@gmail.com>
2016-08-04 16:05:23 +0100

    Added tag as arg

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,10 +6,10 @@ APP_NAME=$1
 SEBASTOPOL_IP=$2
 ENVIRONMENT=$3
 INSTANCE_COUNT=$4
-CANARY=$5
+TAG=$5
+CANARY=$6
 
 # using deployment service sebastopol
-TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
 VPC=sandpit
 sed -e "s/@@TAG@@/$TAG/" -e "s/@@ENVIRONMENT@@/$ENVIRONMENT/" -e "s/@@VPC@@/$VPC/" -e "s/@@CANARY@@/$CANARY/" -e "s/@@APP_NAME@@/$APP_NAME/" -e "s/@@IMAGE_PREFIX@@/$IMAGE_PREFIX/"  -e "s/@@INSTANCE_COUNT@@/$INSTANCE_COUNT/" ./deployment/marathon-config.json.template > $APP_NAME$CANARY.json
 
